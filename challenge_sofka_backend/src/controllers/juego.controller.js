28a54d82e4eb2c7160a15cd695c7a02749e2c9d7
elias <bianchi.elias@gmail.com>
2022-05-06 18:35:32 -0300

    listado ordenado de ranking

--- challenge_sofka_backend/src/controllers/juego.controller.js
+++ challenge_sofka_backend/src/controllers/juego.controller.js
@@ -2,6 +2,10 @@ const juegosSchema = require('../schemas/juegos.schema')
 
 exports.getJuegos = async (req, res) => {
 	const juegos = await juegosSchema.find({})
+
+	juegos.sort((a, b) => {
+		return ( a.puntuacion > b.puntuacion ) ? -1 : (a.puntuacion < b.puntuacion) ? 1 : 0
+	})
 	
-	res.json(juegos)
+	res.json(juegos.slice(0, 5))
 }
